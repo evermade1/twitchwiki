78ed4e6c01cd97db9a476039d3e95d5ff6d65f98
Mooyoung Cho <scmy1013@naver.com>
2023-12-03 08:10:12 +0900

    1202 Information control, Offline search

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Page from "./routes/Page";
 import Loading from "./routes/Loading";
 import MainNavBar from "./routes/Navbar";
 import SearchResult from "./routes/SearchResult";
+import SearchResultOffline from "./routes/SearchResultOffline";
 import {
   BrowserRouter as Router,
   Routes,
@@ -18,17 +19,41 @@ function App() {
   const [moveTo, setMoveTo] = useState(null);
   const [userInfo, setUserInfo] = useState() // 스트리머 정보
   const [streamInfo, setStreamInfo] = useState(null) // 생방송 정보
-  const [streamers, setStreamers] = useState([]) // 페이지가 존재하는 스트리머 정보 담는 배열
+  const storedStreamers = localStorage.getItem('streamers');
+  const [streamers, setStreamers] = useState(storedStreamers ? JSON.parse(storedStreamers) : [])
 
-  useEffect(() => { // 방송 시작 후 24시간이 지난 문서 삭제 (렌더링 시마다 실행)
-    const storedStreamers = localStorage.getItem('streamers');
-    if (storedStreamers) {
-      const tmpStreamers = JSON.parse(storedStreamers)
-      const filteredTmpStreamers = tmpStreamers.filter(item => item.startTime + 86400000 > Date.now())
-      setStreamers(filteredTmpStreamers)
-      localStorage.setItem('streamers', JSON.stringify(filteredTmpStreamers));
-    }
+  useEffect(() => {
+    const updateStreamers = async () => {
+      const storedStreamers = localStorage.getItem('streamers');
+      if (storedStreamers) {
+        const tmpStreamers = JSON.parse(storedStreamers);
+        const updatedStreamers = await Promise.all(tmpStreamers.map(async (streamer) => {
+          const json2 = await (
+            await fetch(`https://api.twitch.tv/helix/streams?user_id=${streamer.digitId}`, { headers: Headers })
+          ).json();
+
+          if (json2.data[0]) {
+            return {
+              ...streamer,
+              viewerCount: json2.data[0].viewer_count,
+            };
+          }
+          return {
+            ...streamer,
+            viewerCount: 0
+          };
+        }));
+
+        const filteredUpdatedStreamers = updatedStreamers.filter(item => item.startTime + 86400000 > Date.now());
+        const sortedUpdatedStreamers = [...filteredUpdatedStreamers].sort((a, b) => b.viewerCount - a.viewerCount) // 시청자수 기준으로 정렬
+        localStorage.setItem('streamers', JSON.stringify(sortedUpdatedStreamers));
+        setStreamers(sortedUpdatedStreamers);
+      }
+    };
+
+    updateStreamers();
   }, []);
+
 
   const getId = async (inputvalue) => { // 스트리머 정보 가져오기
     const json = await (
@@ -44,6 +69,8 @@ function App() {
     return json
   }
   const handleSearchButtonClick = async (searchValue) => { // '검색' 버튼 클릭 시 아이디에 대해 검색 실행
+    setUserInfo()
+    setStreamInfo()
     await getId(searchValue)
   }
   const SearchResultFunction = (makeOrMove, digitId, id, name, startTime, viewerCount, profileImage, userInfo, streamInfo) => { // '생성' 버튼 클릭 시 스트리머 정보 저장
@@ -57,26 +84,25 @@ function App() {
     const newStreamer = { digitId, id, name, startTime, viewerCount, profileImage, userInfo, streamInfo }
     const updatedStreamers = [...streamers, newStreamer] // 이전 배열에 새 오브젝트 추가
     const sortedUpdatedStreamers = [...updatedStreamers].sort((a, b) => b.viewerCount - a.viewerCount) // 시청자수 기준으로 정렬
-
-    setStreamers(sortedUpdatedStreamers);
     localStorage.setItem('streamers', JSON.stringify(sortedUpdatedStreamers));
+    setStreamers(sortedUpdatedStreamers)
   }
   const handleMoveButtonClick = (digitId) => { // '이동' 버튼 클릭 시 해당 스트리머 문서로 이동
     setMoveTo(digitId)
   }
   const handleClearButtonClick = () => { // '초기화' 버튼 클릭 시 로컬 스토리지 및 streamers 배열 초기화
-    setStreamers([])
     localStorage.clear()
+    setStreamers([])
   }
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <MainNavBar onSearch={handleSearchButtonClick} />
-      <Button style={{backgroundColor: "#2E2E2E", width: "100px", marginLeft: "10px", marginTop: "10px", borderColor: "#9146FF"}} onClick={handleClearButtonClick}>초기화</Button>
+      <Button style={{ backgroundColor: "#2E2E2E", width: "100px", marginLeft: "10px", marginTop: "10px", borderColor: "#9146FF" }} onClick={handleClearButtonClick}>초기화</Button>
       <Routes>
         <Route path="/search_result" element={((userInfo && streamInfo) ? (streamInfo.data[0] ? <div><SearchResult userInfo={userInfo} streamInfo={streamInfo} onSearch={SearchResultFunction} /> {streamers && <Streamers onSearch={handleMoveButtonClick} />} </div>
-          : <div><div>생방송 중이 아닙니다.</div> {streamers && <Streamers onSearch={handleMoveButtonClick} />} </div>) 
-          : userInfo ? (userInfo.error ? <div><div>잘못된 아이디입니다.</div> {streamers && <Streamers onSearch={handleMoveButtonClick} />} </div> 
-          : <div><div>데이터가 없습니다.</div> {streamers && <Streamers onSearch={handleMoveButtonClick} />} </div>) : <Loading />)} />
+          : <div><SearchResultOffline userInfo={userInfo} onSearch={SearchResultFunction} /> {streamers && <Streamers onSearch={handleMoveButtonClick} />} </div>)
+          : userInfo ? (userInfo.error ? <div><div>잘못된 아이디입니다.</div> {streamers && <Streamers onSearch={handleMoveButtonClick} />} </div>
+            : <div><div>데이터가 없습니다.</div> {streamers && <Streamers onSearch={handleMoveButtonClick} />} </div>) : <Loading />)} />
         {streamers && <Route path="/" element={<Streamers onSearch={handleMoveButtonClick} />} />}
         <Route path="/page/:moveTo"
           element={moveTo && streamers ? <Page digitId={moveTo} streamers={streamers} /> : <Loading />} />
--- src/routes/SearchResult.js
+++ src/routes/SearchResult.js
@@ -14,12 +14,12 @@ function SearchResult({ userInfo, streamInfo, onSearch }) {
     const startTime = kst.getTime()
     const viewerCount = streamInfo.data[0].viewer_count
     const profileImage = userInfo ? userInfo.data[0].profile_image_url : null
-    const doesStreamerExist = streamers && streamers.some(v => v.digitId === digitId);
+    const doesStreamerExist = streamers ? streamers.some(v => v.digitId === digitId) : false
     const handleMakeButtonClick = () => {
         onSearch(0, digitId, id, name, startTime, viewerCount, profileImage, userInfo, streamInfo)
     }
     const handleMoveButtonClick = () => {
-        onSearch(1, digitId, id, name, startTime, viewerCount, profileImage, userInfo, streamInfo)
+        onSearch(1, digitId)
     }
 
     return <div style={{ width: "90%", marginLeft: "5%", marginTop: "2%" }}>
@@ -35,15 +35,16 @@ function SearchResult({ userInfo, streamInfo, onSearch }) {
         }}>
             <div style={{ padding: "1%", display: "flex", flexDirection: "row" }}>
                 <img src={userInfo.data[0].profile_image_url} style={{ width: '150px', height: '150px', borderRadius: "15px" }}></img>
-                <div style={{ marginLeft: "20px", display: "flex", flexDirection: "column", justifyContent: "space-between"  }}>
+                <div style={{ marginLeft: "20px", display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
                     <div>
-                    <div style={{ fontWeight: 600, fontSize: 24, margin: 5 }}>{name} ({userInfo.data[0].login})</div>
-                    <div style={{ fontSize: 15, marginLeft: 5, marginTop : 10 }}>{streamInfo.data[0].title}</div>
-                    <div style={{ fontSize: 15, marginLeft: 5, marginTop : 5 }}>{streamInfo.data[0].game_name}</div>
+                        <div style={{ fontWeight: 600, fontSize: 24, margin: 5 }}>{name} ({userInfo.data[0].login})</div>
+                        <div style={{ fontSize: 15, marginLeft: 5, marginTop: 10 }}>{streamInfo.data[0].title}</div>
+                        <div style={{ fontSize: 15, marginLeft: 5, marginTop: 5 }}>{streamInfo.data[0].game_name}</div>
+                        
                     </div>
-                    <div style={{ display: "flex", flexDirection: "row", alignItems: "center"}}>
-                    <FaCircle style={{fontSize: "10px", color: "red", marginRight: 5, marginTop: 2}} />
-                    <div>{viewerCount.toLocaleString()}</div>
+                    <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                        <FaCircle style={{ fontSize: "10px", color: "red", marginRight: 5, marginTop: 2 }} />
+                        <div>{viewerCount.toLocaleString()}</div>
                         {doesStreamerExist ?
                             <Link to={`/page/${digitId}`}>
                                 <Button style={{ backgroundColor: "#9146FF", borderColor: "#9146FF", fontSize: 15, marginLeft: "20px" }} onClick={handleMoveButtonClick}>문서로 이동</Button>
@@ -53,7 +54,7 @@ function SearchResult({ userInfo, streamInfo, onSearch }) {
                                 문서 생성하기
                             </Button>
                         }
-                        <Link to={`https://www.twitch.tv/${id}`} style={{marginLeft: "20px"}}>
+                        <Link to={`https://www.twitch.tv/${id}`} style={{ marginLeft: "20px" }}>
                             <Button style={{ backgroundColor: "#9146FF", borderColor: "#9146FF", fontSize: 15 }}>방송으로 이동</Button>
                         </Link>
                     </div>
